1a54cf866d80b39dc9d9e57b72b0def5d657deb7
JMarcosSilva <jmls1912@gmail.com>
2024-04-29 18:02:47 -0300

    correção de exercício

--- Exercícios Java/src/exe005.java
+++ Exercícios Java/src/exe005.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
 
 public class exe005 {
     public static void main(String[] args) {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int prod1, prod2, uni1, uni2;
+
+        int cod1, cod2, qte1, qte2;
         double vlr1, vlr2, total;
 
-        System.out.println("Digite o Código da peça 1: ");
-        prod1 = sc.nextInt();
-        System.out.println("Digite o número de peças: ");
-        uni1 = sc.nextInt();
-        System.out.println("Didgite o valor por peça: ");
+        System.out.print("Digite o Código da peça 1: ");
+        cod1 = sc.nextInt();
+        System.out.print("Digite o número de peças: ");
+        qte1 = sc.nextInt();
+        System.out.print("Didgite o valor por peça: ");
         vlr1 = sc.nextDouble();
 
-        System.out.println("Digite o Código da peça 2: ");
-        prod2 = sc.nextInt();
-        System.out.println("Digite o número de peças: ");
-        uni2 = sc.nextInt();
-        System.out.println("Didgite o valor por peça: ");
+        System.out.print("Digite o Código da peça 2: ");
+        cod2 = sc.nextInt();
+        System.out.print("Digite o número de peças: ");
+        qte2 = sc.nextInt();
+        System.out.print("Didgite o valor por peça: ");
         vlr2 = sc.nextDouble();
 
-        total = (vlr1 * uni1) + (vlr2 * uni2);
+        total = (vlr1 * qte1) + (vlr2 * qte2);
 
-        System.out.printf("Total a pagar: %.2f", total);
+        System.out.println("Código do produto 1: " + cod1);
+        System.out.println("Código do produto 2: " + cod2);
+        System.out.printf("Valor a pagar: %.2f", total);
 
         sc.close();
     }
