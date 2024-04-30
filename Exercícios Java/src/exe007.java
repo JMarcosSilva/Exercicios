dd2ecf6d37491b0fff9ac5401b72c651cdd5921a
JMarcosSilva <jmls1912@gmail.com>
2024-04-30 14:58:57 -0300

    adição de exercício

---
+++ Exercícios Java/src/exe007.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exe007 {
+    public static void main(String[] args) {
+        int n = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Número impar");
+        }else
+            System.out.println("número Par");
+
+        sc.close();
+        
+    }
+}
