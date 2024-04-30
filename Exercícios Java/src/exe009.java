31065e3c2f1ce988e7cad4c9338ca8c2963ac0f2
JMarcosSilva <jmls1912@gmail.com>
2024-04-30 17:47:59 -0300

    adição de exercícios

---
+++ Exercícios Java/src/exe009.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exe009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.print("Digite um número: ");
+        a = sc.nextInt();
+        System.out.print("Digite outro número: ");
+        b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("São multiplos");
+        }else
+            System.out.println("Não são multiplos");
+
+        sc.close();
+    }
+    
+}
